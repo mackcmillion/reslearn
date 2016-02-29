fixed some issues in meanstddev computation

config.py
@@ -25,13 +25,13 @@ tf.app.flags.DEFINE_string('adjust_dimensions_strategy', 'A',
 OPTIMIZER = tf.train.MomentumOptimizer
 OPTIMIZER_ARGS = {'momentum': 0.9}
 
-tf.app.flags.DEFINE_float('initial_learning_rate', 0.01,
+tf.app.flags.DEFINE_float('initial_learning_rate', 0.1,
                           """
                           The initial learning rate. May be decayed over time by the selected learning rate
                           decay strategy.
                           """)
 
-tf.app.flags.DEFINE_integer('learning_rate_decay_strategy', 1,
+tf.app.flags.DEFINE_integer('learning_rate_decay_strategy', 0,
                             """
                             The schedule that is used to decay the learning rate. Possible options:
                             - 0 Divides the learning rate by 10 at 32000 and 48000 steps.

datasets/cifar10.py
@@ -31,7 +31,10 @@ class Cifar10(Dataset):
         return self._inputs(False)
 
     def _inputs(self, is_training):
-        filenames = [os.path.join(FLAGS.cifar10_image_path, 'data_batch_%i.bin' % i) for i in xrange(1, 6)]
+        if is_training:
+            filenames = [os.path.join(FLAGS.cifar10_image_path, 'data_batch_%i.bin' % i) for i in xrange(1, 6)]
+        else:
+            filenames = [os.path.join(FLAGS.cifar10_image_path, 'test_batch.bin')]
         filename_queue = tf.train.string_input_producer(filenames, name='%s_filename_queue' %
                                                                         'training' if is_training else 'evaluation')
 
@@ -42,7 +45,8 @@ class Cifar10(Dataset):
         else:
             image = self._preprocess_for_evaluation(image)
 
-        min_num_examples_in_queue = int(FLAGS.min_frac_examples_in_queue * self._NUM_TRAINING_IMAGES)
+        min_num_examples_in_queue = int(FLAGS.min_frac_examples_in_queue *
+                                        self._NUM_TRAINING_IMAGES if is_training else self._NUM_VALIDATION_IMAGES)
         image_batch, label_batch = tf.train.shuffle_batch(
                 [image, label],
                 batch_size=FLAGS.batch_size,

datasets/imagenet.py
@@ -77,7 +77,8 @@ class ImageNet(Dataset):
         # prepare image
         image = tf.image.decode_jpeg(image, channels=3)
         feature_img = tf.cast(image, tf.float32)
-        processed_img = self._preprocess_for_training(feature_img)
+        relative_img = util.absolute_to_relative_colors(feature_img)
+        processed_img = self._preprocess_for_training(relative_img)
 
         return [processed_img, label]
 

preprocess.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow.python.ops import control_flow_ops as cf
 
+from util import replicate_to_image_shape
+
 
 def resize_random(image, minval, maxval_inc):
     new_shorter_edge_tensor = tf.random_uniform([], minval=minval, maxval=maxval_inc + 1, dtype=tf.int32)
@@ -59,25 +61,17 @@ def color_noise(image, eigvals, eigvecs):
     q = tf.matmul(eigvecs, tf.expand_dims(alpha * eigvals, 1))
     q = tf.squeeze(q)
 
-    return image + _replicate_to_image_shape(image, q)
+    return image + replicate_to_image_shape(image, q)
 
 
 def normalize_colors(image, mean, stddev):
     mean = tf.constant(mean, dtype=tf.float32)
     stddev = tf.constant(stddev, dtype=tf.float32)
 
-    mean = _replicate_to_image_shape(image, mean)
-    stddev = _replicate_to_image_shape(image, stddev)
+    mean = replicate_to_image_shape(image, mean)
+    stddev = replicate_to_image_shape(image, stddev)
 
     return (image - mean) / stddev
-
-
-def _replicate_to_image_shape(image, t):
-    img_shape = tf.shape(image)
-    multiples = tf.pack([img_shape[0], img_shape[1], 1])
-    t = tf.expand_dims(tf.expand_dims(t, 0), 0)
-    t = tf.tile(t, multiples)
-    return t
 
 
 def ten_crop(image):

scripts/meanstddev.py
@@ -3,31 +3,33 @@ import os
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 
+import util
 from config import FLAGS
 from scripts.labelmap import build_filename_list
 
 
 def compute_overall_mean_stddev(overwrite=False, num_threads=1, num_logs=10):
     if FLAGS.dataset == 'cifar10':
-        # TODO implement for test set
-        # TODO implement relative color values (divide by 256)
         _compute_overall_mean_stddev(overwrite, num_threads, num_logs,
                                      image_op=_image_op_cifar10,
                                      filenames=[os.path.join(FLAGS.cifar10_image_path, 'data_batch_%i.bin' % i) for i in
-                                                xrange(1, 6)],
+                                                xrange(1, 6)] +
+                                               [os.path.join(FLAGS.cifar10_image_path, 'test_batch.bin')],
                                      mean_stddev_path=FLAGS.cifar10_mean_stddev_path,
-                                     num_files=50000)
+                                     relative_colors=False,
+                                     num_files=60000)
     elif FLAGS.dataset == 'imagenet':
         _compute_overall_mean_stddev(overwrite, num_threads, num_logs,
                                      filenames=build_filename_list(),
                                      mean_stddev_path=FLAGS.mean_stddev_path,
+                                     relative_colors=True,
                                      image_op=_image_op_imagenet)
     else:
         raise ValueError('Unknown dataset.')
 
 
 def _compute_overall_mean_stddev(overwrite, num_threads, num_logs, image_op, filenames, mean_stddev_path,
-                                 num_files=None):
+                                 relative_colors, num_files=None):
     if gfile.Exists(mean_stddev_path):
         print 'Mean/stddev file already exists.'
         if overwrite:
@@ -41,7 +43,7 @@ def _compute_overall_mean_stddev(overwrite, num_threads, num_logs, image_op, fil
     mean = tf.Variable([0.0, 0.0, 0.0], trainable=False)
     total = tf.Variable(0.0, trainable=False)
 
-    image = image_op(filenames)
+    image = image_op(filenames, relative_colors)
 
     # mean computation
     mean_ops = _mean_ops(image, mean, total, num_threads)
@@ -54,7 +56,7 @@ def _compute_overall_mean_stddev(overwrite, num_threads, num_logs, image_op, fil
     covariance = tf.Variable(tf.zeros([3, 3], dtype=tf.float32), trainable=False)
     total = tf.Variable(0.0, trainable=False)
 
-    image = image_op(filenames)
+    image = image_op(filenames, relative_colors)
 
     covariance_ops = _covariance_ops(image, covariance, total, mean, num_threads)
     stddev, eigvals, eigvecs = _init_and_run_in_loop(covariance_ops, 'standard deviation', _covariance_final_ops,
@@ -77,15 +79,19 @@ def _compute_overall_mean_stddev(overwrite, num_threads, num_logs, image_op, fil
     tf.reset_default_graph()
 
 
-def _image_op_imagenet(filenames):
+def _image_op_imagenet(filenames, relative_colors):
     filename_queue = tf.train.string_input_producer(filenames, num_epochs=1)
     reader = tf.WholeFileReader()
     _, value = reader.read(filename_queue)
     image = tf.image.decode_jpeg(value, channels=3)
-    return tf.cast(image, tf.float32)
+    image = tf.cast(image, tf.float32)
+
+    if relative_colors:
+        image = util.absolute_to_relative_colors(image)
+    return image
 
 
-def _image_op_cifar10(filenames):
+def _image_op_cifar10(filenames, relative_colors):
     label_bytes = 1
     height = 32
     width = 32
@@ -99,7 +105,11 @@ def _image_op_cifar10(filenames):
     record_bytes = tf.decode_raw(value, tf.uint8)
     depth_major = tf.reshape(tf.slice(record_bytes, [label_bytes], [image_bytes]), [depth, height, width])
     image = tf.transpose(depth_major, [1, 2, 0])
-    return tf.cast(image, tf.float32)
+    image = tf.cast(image, tf.float32)
+
+    if relative_colors:
+        image = util.absolute_to_relative_colors(image)
+    return image
 
 
 def _mean_ops(image, mean, total, num_threads):
@@ -118,11 +128,9 @@ def _mean_ops(image, mean, total, num_threads):
 
 def _covariance_ops(image, covariance, total, mean, num_threads):
     num = tf.mul(tf.shape(image)[0], tf.shape(image)[1])
-
-    mean_tiled = tf.tile(mean, tf.expand_dims(num, 0))
-    mean_tiled = tf.reshape(mean_tiled, tf.pack([tf.shape(image)[0], tf.shape(image)[1], 3]))
-
     num = tf.cast(num, tf.float32)
+
+    mean_tiled = util.replicate_to_image_shape(image, mean)
 
     remainders = tf.sub(image, mean_tiled)
     remainders_stack = tf.pack([remainders, remainders, remainders])
@@ -187,7 +195,7 @@ def _covariance_final_ops(sum_squares, total):
     total_3x3 = tf.reshape(tf.tile(tf.expand_dims(total, 0), [9]), [3, 3])
     covariance = tf.div(sum_squares, total_3x3)
 
-    variance = tf.gather(tf.reshape(covariance, [-1]), [0, 3, 6])
+    variance = tf.gather(tf.reshape(covariance, [-1]), [0, 4, 8])
 
     # eigenvalues and eigenvectors for PCA
     eigens = tf.self_adjoint_eig(covariance)