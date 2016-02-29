76aad61e2a59fb85d22619e98d1bbf0945290d86
Max Mumme <maxx.mumme@gmx.de>
2016-02-29 16:04:21 +0100

    simplified path parameters

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+import os
+
 import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
 
 # global run config
 tf.app.flags.DEFINE_string('experiment_name', 'resnet_34_test',
@@ -19,7 +23,6 @@ tf.app.flags.DEFINE_string('adjust_dimensions_strategy', 'A',
                                enable residual learning but increase complexity.
                            Option C, using projections even if there is no need to adjust dimensions, is not supported.
                            """)
-
 
 # constants specifying training and validation behaviour
 OPTIMIZER = tf.train.MomentumOptimizer
@@ -52,7 +55,6 @@ tf.app.flags.DEFINE_float('min_frac_examples_in_queue', 0.1,
                           """The minimum fraction of all examples to be held in the input queue.
                           Ensures good shuffling.""")
 
-# TODO balance consuming threads
 tf.app.flags.DEFINE_integer('num_consuming_threads', 3,
                             """Number of threads consuming a filename to produce an image example.""")
 
@@ -77,40 +79,41 @@ tf.app.flags.DEFINE_boolean('run_once', False,
 tf.app.flags.DEFINE_integer('top_k', 1,
                             """When evaluating, compute the top-k-error.""")
 
+# base data directory
+tf.app.flags.DEFINE_string('data_path', '/home/max/Studium/Kurse/BA2/data',
+                           """Directory that contain all the data.""")
 
 # ImageNet data directory and file paths
-tf.app.flags.DEFINE_string('training_images', '/home/max/Studium/Kurse/BA2/data/imagenet/synsets',
+tf.app.flags.DEFINE_string('training_images', os.path.join(FLAGS.data_path, 'imagenet/synsets'),
                            """Directory containing the training image data.""")
 
-tf.app.flags.DEFINE_string('validation_images', '/home/max/Studium/Kurse/BA2/data/imagenet/validation',
+tf.app.flags.DEFINE_string('validation_images', os.path.join(FLAGS.data_path, 'imagenet/validation'),
                            """Directory containing the validation image data.""")
 
-tf.app.flags.DEFINE_string('wnid_lid_path', '/home/max/Studium/Kurse/BA2/data/imagenet/map_clsloc.txt',
+tf.app.flags.DEFINE_string('wnid_lid_path', os.path.join(FLAGS.data_path, 'imagenet/map_clsloc.txt'),
                            """The file where to load the WNID_LID_MAP from.""")
 
-tf.app.flags.DEFINE_string('mean_stddev_path', '/home/max/Studium/Kurse/BA2/data/imagenet/mean_stddev',
+tf.app.flags.DEFINE_string('mean_stddev_path', os.path.join(FLAGS.data_path, 'imagenet/mean_stddev'),
                            """Path where to store/load precomputed mean/stddev over a whole dataset.""")
 
-tf.app.flags.DEFINE_string('training_set', '/home/max/Studium/Kurse/BA2/data/imagenet/labelmap',
+tf.app.flags.DEFINE_string('training_set', os.path.join(FLAGS.data_path, 'imagenet/labelmap'),
                            """Path to the file mapping each filename to its label.""")
 
 tf.app.flags.DEFINE_string('validation_set',
-                           "/home/max/Studium/Kurse/BA2/data/imagenet/ILSVRC2015_clsloc_ground_truth.txt",
+                           os.path.join(FLAGS.data_path, 'imagenet/ILSVRC2015_clsloc_ground_truth.txt'),
                            """Path to the validation label map.""")
 
 tf.app.flags.DEFINE_string('validation_blacklist',
-                           "/home/max/Studium/Kurse/BA2/data/imagenet/ILSVRC2015_clsloc_validation_blacklist.txt",
+                           os.path.join(FLAGS.data_path, 'imagenet/ILSVRC2015_clsloc_validation_blacklist.txt'),
                            """Path to the validation set blacklist.""")
 
-
 # CIFAR-10 data directory and file paths
-tf.app.flags.DEFINE_string('cifar10_image_path', '/home/max/Studium/Kurse/BA2/data/cifar-10-batches-bin',
+tf.app.flags.DEFINE_string('cifar10_image_path', os.path.join(FLAGS.data_path, 'cifar-10-batches-bin'),
                            """Path to training and test images for CIFAR-10 dataset.""")
 
 tf.app.flags.DEFINE_string('cifar10_mean_stddev_path',
-                           '/home/max/Studium/Kurse/BA2/data/cifar-10-batches-bin/mean_stddev',
+                           os.path.join(FLAGS.data_path, 'cifar-10-batches-bin/mean_stddev'),
                            """Path where to store/load precomputed mean/stddev over a whole CIFAR-10 dataset.""")
-
 
 # target directory and file paths
 tf.app.flags.DEFINE_string('summary_path', '/home/max/Studium/Kurse/BA2/summaries',
@@ -119,5 +122,3 @@ tf.app.flags.DEFINE_string('summary_path', '/home/max/Studium/Kurse/BA2/summarie
 tf.app.flags.DEFINE_string('checkpoint_path',
                            '/home/max/Studium/Kurse/BA2/checkpoints',
                            """Path to periodically save checkpoints of the training procedure.""")
-
-FLAGS = tf.app.flags.FLAGS
