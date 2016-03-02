202ec680ea110156612eacd9f28961c938c364f0
Max Mumme <maxx.mumme@gmx.de>
2016-03-02 15:51:37 +0100

    hopefully fixed logs stuff for the last time

--- batchfiles/batchfiles/resnet_20.sh
+++ batchfiles/batchfiles/resnet_20.sh
@@ -2,8 +2,7 @@
 
 echo "Started computation for resnet_20..."
 
-python /root/reslearn/main.py --experiment_name=resnet_20 --dataset=cifar10 --model=cifar10-resnet-20 \
- --data_path=/root/data --summary_path=/root/summaries --checkpoint_path=/root/checkpoints \
- &> /root/logs/resnet_20.log
+stdbuf -oL python /root/reslearn/main.py --experiment_name=resnet_20 --dataset=cifar10 --model=cifar10-resnet-20 \
+ --data_path=/root/data --summary_path=/root/summaries --checkpoint_path=/root/checkpoints &>/root/logs/resnet_20.log
 
 echo "Finished computation for resnet_20."
