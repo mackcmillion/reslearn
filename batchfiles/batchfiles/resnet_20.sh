9c3c27d5d3057288691c6aa7dde7c67b81b67429
Max Mumme <maxx.mumme@gmx.de>
2016-03-02 15:59:32 +0100

    definitely fixed for the last time

--- batchfiles/batchfiles/resnet_20.sh
+++ batchfiles/batchfiles/resnet_20.sh
@@ -3,6 +3,6 @@
 echo "Started computation for resnet_20..."
 
 stdbuf -oL python /root/reslearn/main.py --experiment_name=resnet_20 --dataset=cifar10 --model=cifar10-resnet-20 \
- --data_path=/root/data --summary_path=/root/summaries --checkpoint_path=/root/checkpoints &>/root/logs/resnet_20.log
+ --data_path=/root/data --summary_path=/root/summaries --checkpoint_path=/root/checkpoints >/root/logs/resnet_20.log
 
 echo "Finished computation for resnet_20."
