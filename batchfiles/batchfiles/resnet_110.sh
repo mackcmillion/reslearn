46d53be5f78f2170b0ffd4f0def8ca2749cbfe6c
Max Mumme <maxx.mumme@gmx.de>
2016-03-11 21:52:39 +0100

    updated resnet110.sh for training on fishermans-friend... hope for the best

--- batchfiles/batchfiles/resnet_110.sh
+++ batchfiles/batchfiles/resnet_110.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir /root/logs
+mkdir /home/max/logs
 
 echo "Started computation for resnet_110..."
 
-stdbuf -oL python /root/reslearn/main.py --experiment_name=resnet_110 --dataset=cifar10 --model=cifar10-resnet-110 \
- --data_path=/root/data --summary_path=/root/summaries --checkpoint_path=/root/checkpoints >/root/logs/resnet_110.log
+stdbuf -oL python /home/max/reslearn/main.py --experiment_name=resnet_110 --dataset=cifar10 --model=cifar10-resnet-110 \
+ --data_path=/home/max/data --summary_path=/home/max/summaries --checkpoint_path=/home/max/checkpoints >/home/max/logs/resnet_110.log
 
 echo "Finished computation for resnet_110."
