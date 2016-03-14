9fdffe5d77cfffaecce530083df9588a0d19fb05
Max Mumme <maxx.mumme@gmx.de>
2016-03-14 13:42:21 +0100

    added faster eval polling

--- batchfiles/batchfiles/resnet_56.sh
+++ batchfiles/batchfiles/resnet_56.sh
@@ -13,7 +13,7 @@
 echo "Started evaluation for resnet_56."
 
 stdbuf -oL python /home/max/reslearn/main.py --experiment_name=resnet_56 --dataset=cifar10 --model=cifar10-resnet-56 \
- --eval \
+ --eval --eval_interval_secs=1 \
  --data_path=/home/max/data --summary_path=/home/max/summaries --checkpoint_path=/home/max/checkpoints >/home/max/logs/resnet_56_eval.log
 
 echo "Finished evaluation for resnet_56."
