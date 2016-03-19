hopefully implemented correct LR scaling

config.py
@@ -36,10 +36,10 @@ tf.app.flags.DEFINE_string('adjust_dimensions_strategy', 'A',
 # constants specifying training and validation behaviour
 # TODO try momentum = 0.1 * 0.9 and LR = 0.1
 # OPTIMIZER = tf.train.MomentumOptimizer
-# OPTIMIZER_ARGS = {'momentum': 0.9}
+# OPTIMIZER_ARGS = {'momentum': 0.1 * 0.9}
 # OPTIMIZER_ARGS = {'epsilon': 0.1}
 
-tf.app.flags.DEFINE_float('initial_learning_rate', 0.01,
+tf.app.flags.DEFINE_float('initial_learning_rate', 0.1,
                           """
                           The initial learning rate. May be decayed over time by the selected learning rate
                           decay strategy.

train.py
@@ -159,7 +159,7 @@ def training_op(total_loss, train_err, train_err_assign, global_step):
 
     with tf.control_dependencies([train_err_avg_op, train_err_assign]):
         with tf.control_dependencies([loss_averages_op, lr_decay_op]):
-            optimizer = tf.train.MomentumOptimizer(lr, momentum=tf.mul(lr, 0.9))
+            optimizer = tf.train.MomentumOptimizer(lr, momentum=tf.div(0.9, lr))
             grads = optimizer.compute_gradients(total_loss)
 
     apply_gradient_op = optimizer.apply_gradients(grads, global_step=global_step)