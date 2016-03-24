0a5287ed47b39ba1904845abff86307480a4d0c3
Max Mumme <maxx.mumme@gmx.de>
2016-03-24 11:43:33 +0100

    fixed learning rate schedule... fuck weak typing

--- learningrate.py
+++ learningrate.py
@@ -8,7 +8,7 @@ def update_lr(sess, lr, global_step, train_err_avg):
     if FLAGS.learning_rate_decay_strategy == 0:
         return decay_at_fixed_steps_default(lr, global_step)
     elif FLAGS.learning_rate_decay_strategy == 1:
-        train_err = sess.run([train_err_avg])
+        train_err = sess.run(train_err_avg)
         return raise_at_train_err_then_decay_at_fixed_steps_default(lr, global_step, train_err)
     else:
         raise ValueError('Unknown learning rate decay strategy.')
@@ -25,7 +25,7 @@ def decay_at_fixed_steps(lr, global_step, thresholds, decay_factor):
 
 
 def raise_at_train_err_then_decay_at_fixed_steps_default(lr, global_step, train_err):
-    return raise_at_train_err_then_decay_at_fixed_steps(lr, global_step, [52000, 92000], 0.1, train_err, 0.76, 0.1)
+    return raise_at_train_err_then_decay_at_fixed_steps(lr, global_step, [52000, 92000], 0.1, train_err, 0.8, 0.1)
 
 
 def raise_at_train_err_then_decay_at_fixed_steps(lr, global_step, thresholds, decay_factor, train_err, err_thresh,
